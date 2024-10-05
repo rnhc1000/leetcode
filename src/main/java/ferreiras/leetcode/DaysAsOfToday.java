42ededd5f2c8320745e40b631b1f05484380944e
Ricardo Ferreira <rnhc1000@hotmail>
2024-10-05 14:44:36 -0300

    day278of24 - String permutation - frequency = int[] f1 = new int[26] for (int i; i < sizeOfString;  i++) f1[s.charAt(i) -'a']++ to get the frequenc of any character

--- src/main/java/ferreiras/leetcode/DaysAsOfToday.java
+++ src/main/java/ferreiras/leetcode/DaysAsOfToday.java
@@ -1,4 +1,35 @@
 package ferreiras.leetcode;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class DaysAsOfToday {
+
+  private static final String DATE_FORMAT = "dd/MM/yyyy";
+  private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
+
+  public static void main(String[] args) {
+
+    String firstDay = "01/01/2024";
+    String currentDay = LocalDateTime.now().format(formatter);
+    int response = getDaysOfDifference(firstDay, currentDay);
+    System.out.println("Day " + response + " of 365");
+  }
+
+  public static int getYearsOfDifference(String start, String end) {
+
+    int firstDay = LocalDate.parse(start, formatter).getYear();
+    int currentDay = LocalDate.parse(end, formatter).getYear();
+
+    return currentDay - firstDay;
+  }
+
+  public static int getDaysOfDifference(String start, String end) {
+
+    int firstDay = LocalDate.parse(start, formatter).getDayOfYear();
+    int currentDay = LocalDate.parse(end, formatter).getDayOfYear();
+
+    return currentDay - firstDay;
+  }
 }
